9b35e2c0fc0b157c0c6758e11b95b6122e373723
hamzza-K <34022534+hamzza-K@users.noreply.github.com>
2018-12-03 21:22:11 +0500

    Add files via upload

---
+++ renaming_files_with_os.py
@@ -0,0 +1,58 @@
+import os
+import random
+try:
+    os.chdir('C:/Users/HP/Desktop/experiment')
+except Exception as e:
+    os.mkdir('C:/Users/HP/Desktop/experiment')
+print(os.getcwd())
+
+names = ['this sucks', 'lmao', 'omaewashinderu','meh','Ahegao']
+my_list = ['wahahhah', 'what are you looking at', 'you moron', 'i am watching you']
+
+class Mi_hacker(object):
+
+
+    def changing_files(self):
+        for f in os.listdir():
+            f_name, f_ext = os.path.splitext(f)
+            nums, names = f_name.split('-')
+            nums = nums.zfill(2)
+            # names = names.strip()[:-2]
+            new_name = '{}-{}{}'.format(names, nums, f_ext)
+            os.rename(f, new_name)
+            # print(new_name)
+    @staticmethod
+    def writing_new_files():
+        for i in range(10):
+            with open('{}-{}.txt'.format(i, random.choice(names)), 'w') as f:
+                f.write(random.choice(my_list))
+
+    def deleting_new_files(self):
+        new = []
+        for f in os.listdir():
+            fname, fext = os.path.splitext(f)
+            if fname.endswith('eh'):
+                new.append(f)
+
+        print(new)
+        for f in new:
+            if os.path.isfile(f):
+                os.remove(f)
+    @staticmethod
+    def deleting_all_files():
+        for f in os.listdir():
+            os.remove(f)
+
+if os.listdir():
+    pass
+else:
+    Mi_hacker.writing_new_files()
+
+
+
+migood = Mi_hacker()
+
+
+# migood.writing_new_files()
+# migood.changing_files()
+migood.deleting_all_files()
